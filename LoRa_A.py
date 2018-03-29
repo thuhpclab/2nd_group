5e9d5cc79cd98e537b36bcd7ff60e20928b560ad
thuhpclab <37896058+thuhpclab@users.noreply.github.com>
2018-03-29 16:13:18 +0800

    Add files via upload

---
+++ LoRa_A.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+import urllib3
+import MySQLdb as mysql
+import os,time,requests,sys,datetime
+reload(sys)#重要編碼
+from threading import Timer
+from datetime import date
+from bs4 import *
+urllib3.disable_warnings()
+sys.setdefaultencoding('utf8')
+conn=mysql.connect(host="140.128.101.210",user="hpc",passwd="hpc123",db="Environment",charset="utf8")
+cursor=conn.cursor()
+def is_integer(x):
+    try:
+        return float(x)
+    except ValueError:
+        return -404.0
+url_A='https://api.thingspeak.com/channels/241665/feeds/last.xml?timezone=Asia/Taipei'
+#url_B='https://api.thingspeak.com/channels/256131/feeds/last.xml?timezone=Asia/Taipei'
+#url_C='https://api.thingspeak.com/channels/306218/feeds/last.xml?timezone=Asia/Taipei'
+#url_D='https://api.thingspeak.com/channels/330089/feeds/last.xml?timezone=Asia/Taipei'
+html=requests.get(url_A)
+html.encoding='utf-8'
+sp=BeautifulSoup(html.text,'html.parser')
+tab_66=sp.find("feed")
+time=tab_66.find("created-at").string
+time=time.strip("+08:00")
+time=time.replace("T"," ")
+
+date=time.replace("-","/")
+nid=tab_66.find("entry-id").string
+hum=tab_66.find("field1").string
+temp=tab_66.find("field2").string
+PM25=tab_66.find("field3").string
+PM10=tab_66.find("field4").string
+
+cursor.execute("SET NAMES UTF8")
+select_sql="select * from LoRa where `timestamp`= %s and `sensor`='A'"
+cursor.execute(select_sql,date)
+result=cursor.fetchall()
+conn.commit()
+a = 'A'
+if result==():
+	insert_sql="insert into LoRa values('{}',{},{},{},{},'{}','{}')".format(a,temp,hum,PM25,PM10,date,nid)
+	cursor.execute(insert_sql)
+	conn.commit()
+else:
+	conn.close()
+#print(date)
+
---
+++ observation_traffic.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+import urllib3
+import MySQLdb as mysql
+import os,time,requests,sys,datetime
+reload(sys)#重要編碼
+from threading import Timer
+from datetime import date
+from bs4 import *
+urllib3.disable_warnings()
+sys.setdefaultencoding('utf8')
+
+conn=mysql.connect(host="140.128.101.210",user="hpc",passwd="hpc123",db="Environment",charset="utf8")
+cursor=conn.cursor()
+def is_integer(x):
+    try:
+        return float(x)
+    except ValueError:
+        return -404.0
+
+url_Time='http://taqm.epb.taichung.gov.tw/TQAMNEWAQITABLE.ASPX'
+
+
+url_Observation='http://taqm.epb.taichung.gov.tw/aqi/aqiTRAFFIC.ASPX'
+html=requests.get(url_Observation)
+html.encoding='utf-8'
+sp=BeautifulSoup(html.text,'html.parser')
+
+html2=requests.get(url_Time)
+html2.encoding='utf-8'
+sp2=BeautifulSoup(html2.text,'html.parser')
+Year=sp2.find_all('span')[1].string[16:20]
+Mon=sp2.find_all('span')[1].string[21:23]
+Day=sp2.find_all('span')[1].string[24:26]
+Hour=sp2.find_all('span')[1].string[28:30]
+
+SiteName=sp.find_all('td')[3].string[0:6]
+County=u'臺中市'
+if (sp.find_all('td')[5].string) == '0':
+    AQI = ''
+else:
+    AQI = sp.find_all('td')[5].string
+
+Pollutant=sp.find_all('td')[6].string
+Status=sp.find_all('td')[4].string
+
+if (sp.find_all('td')[8].string) == '0.0':
+    SO2 = ''
+else:
+    SO2 = sp.find_all('td')[8].string
+
+if (sp.find_all('td')[10].string) == '0.00':
+    CO =""
+else:
+    CO = sp.find_all('td')[10].string
+
+
+CO_8hr=""
+
+if (sp.find_all('td')[12].string) == '0.0':
+    O3 = ''
+else:
+    O3 = sp.find_all('td')[12].string
+
+O3_8hr=""
+
+if (sp.find_all('td')[14].string) == '0':
+    PM10 = ''
+else:
+    PM10 = sp.find_all('td')[14].string
+
+if (sp.find_all('td')[18].string) == '0':
+    PM25 = ''
+else:
+    PM25 = sp.find_all('td')[18].string
+
+if (sp.find_all('td')[16].string) == '0.0':
+    NO2 = ''
+else:
+    NO2 = sp.find_all('td')[16].string
+NOx=""
+NO=""
+WindSpeed=""
+WindDirec=""
+
+PublishTime=Year+"-"+Mon+"-"+Day+" "+Hour+":00"
+PM10_AVG=""
+PM25_AVG=""
+
+#print(url_Observation)
+#print SiteName,County,AQI,Pollutant,Status,SO2,CO,CO_8hr,O3,O3_8hr,PM10,PM25,NO2,NOx,NO,WindSpeed,WindDirec,PublishTime,PM10_AVG,PM25_AVG
+
+cursor.execute("SET NAMES UTF8")
+select_sql="select * from airquality where `PublishTime`= %s and `SiteName`= %s "
+cursor.execute(select_sql,(PublishTime,SiteName,))
+result=cursor.fetchall()
+conn.commit()
+SiteName = SiteName
+if result==():
+    insert_sql="insert into airquality(id,SiteName,County,AQI,Pollutant,Status,SO2,CO,CO_8hr,O3,O3_8hr,PM10,PM25,NO2,NOx,NO,WindSpeed,WindDirec,PublishTime,PM10_AVG,PM25_AVG) values (null, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    data= SiteName,County,AQI,Pollutant,Status,SO2,CO,CO_8hr,O3,O3_8hr,PM10,PM25,NO2,NOx,NO,WindSpeed,WindDirec,PublishTime,PM10_AVG,PM25_AVG
+    cursor.execute(insert_sql,data)
+    conn.commit()
+else:
+    conn.close()
